9e33e964a3a45506012d6fa1f00b90e60f93b5f4
EC2 Default User <ec2-user@ip-172-31-5-192.ap-northeast-1.compute.internal>
2022-10-13 15:06:03 +0000

    [Update]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,14 +4,27 @@ class BooksController < ApplicationController
   end
   
   def create
+    @book = Book.new(book_params)
+    @book.save
+    @book.user_id = current_user.id
+      redirect_to book_path(current_book)
   end
   
   def index
    @books = Book.all
-   @book = Book.find(params[:id])
+   @user= current_user
   end
   
   def show
+    @book= Book.find(params[:id])
+    @user= current_user
   end
   
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+ 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,31 @@
 class UsersController < ApplicationController
   def show
+    @users=User.all
     @user= User.find(params[:id])
     
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
   def index
     @users= User.all
+    @user= current_user
   end
   
   def update
+    @user = User.find(params[:id])
+    if @user.update(users_params)
+     redirect_to user_url(@user)
+    else
+     render user_path(@user.id)
+    end
+  end
+  
+    private
+
+  def users_params
+    params.require(:user).permit(:name, :image, :profile)
   end
 end
